aa4ffcc17d301a128cb9ba14de2cc5336d899f14
Dharshib <Dharshib.8@gmail.com>
2024-02-01 08:19:16 +0530

    add user entity

--- backend/src/main/java/com/seng22212project/bitebliss/models/User.java
+++ backend/src/main/java/com/seng22212project/bitebliss/models/User.java
@@ -1,4 +1,62 @@
 package com.seng22212project.bitebliss.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    @Size(max = 64)
+    private String verificationCode;
+
+    private Date verificationCodeExpiryTime;
+    private boolean enabled;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(  name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.verificationCode = null;
+        this.verificationCodeExpiryTime = null;
+        this.enabled = false;
+    }
 }
