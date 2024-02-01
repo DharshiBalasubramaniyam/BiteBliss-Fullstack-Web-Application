881a74ac860f724295fc04414b2238c10ecc0d24
Dharshib <Dharshib.8@gmail.com>
2024-02-01 07:31:47 +0530

    initiate backend of the project

---
+++ backend/src/main/java/com/seng22212project/bitebliss/BiteblissApplication.java
@@ -0,0 +1,13 @@
+package com.seng22212project.bitebliss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteblissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteblissApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/seng22212project/bitebliss/BiteblissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seng22212project.bitebliss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteblissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
