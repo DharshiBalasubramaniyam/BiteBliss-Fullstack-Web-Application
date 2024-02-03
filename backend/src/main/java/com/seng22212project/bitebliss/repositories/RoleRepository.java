f27f6925519c6fdc73f18a8c3fe80ddd32d64400
Dharshib <Dharshib.8@gmail.com>
2024-02-03 11:55:30 +0530

    add role repository

---
+++ backend/src/main/java/com/seng22212project/bitebliss/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.seng22212project.bitebliss.repositories;
+
+import com.seng22212project.bitebliss.models.ERole;
+import com.seng22212project.bitebliss.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByRoleName(ERole roleName);
+}
