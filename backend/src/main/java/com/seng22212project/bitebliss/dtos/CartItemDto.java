08adc428c1f068686a74a3a638f76f0224ed9c8d
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-02-06 17:27:01 +0530

    Added CartItemDto

---
+++ backend/src/main/java/com/seng22212project/bitebliss/dtos/CartItemDto.java
@@ -0,0 +1,52 @@
+package com.seng22212project.bitebliss.dtos;
+
+import jakarta.persistence.Id;
+
+public class CartItemDto {
+    @Id
+    private long cartItemId;
+    private int quantity;
+    private double totalPrice;
+    private CartDto cartDto;
+    private ProductDto productDto;
+
+    public long getCartItemId() {
+        return cartItemId;
+    }
+
+    public void setCartItemId(long cartItemId) {
+        this.cartItemId = cartItemId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public CartDto getCartDto() {
+        return cartDto;
+    }
+
+    public void setCartDto(CartDto cartDto) {
+        this.cartDto = cartDto;
+    }
+
+    public ProductDto getProductDto() {
+        return productDto;
+    }
+
+    public void setProductDto(ProductDto productDto) {
+        this.productDto = productDto;
+    }
+}
