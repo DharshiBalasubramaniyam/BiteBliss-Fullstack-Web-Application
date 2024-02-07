291dd1b0be7a57fa3df8f5a28d8d2a6d55a2b401
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-02-07 17:47:30 +0530

    Resolve conflict at CartController

--- backend/src/main/java/com/seng22212project/bitebliss/controller/CartController.java
+++ backend/src/main/java/com/seng22212project/bitebliss/controller/CartController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.seng22212project.bitebliss.service.CartService;
+
 import com.seng22212project.bitebliss.dtos.CartDto;
 import com.seng22212project.bitebliss.payload.ItemRequest;
 
@@ -14,7 +16,7 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
-    @PostMapping
+    @PostMapping("/")
     public ResponseEntity<CartDto> addToCart(@RequestBody ItemRequest itemRequest, Principal principal){
         CartDto addItem = this.cartService.addItem(itemRequest,principal.getName());
         return new ResponseEntity<CartDto>(addItem, HttpStatus.OK);
