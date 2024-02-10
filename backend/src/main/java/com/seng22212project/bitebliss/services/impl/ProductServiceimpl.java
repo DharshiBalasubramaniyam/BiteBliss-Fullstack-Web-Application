56b940c96ca3c9b357ce3e1dacac4169372c7dda
poojak <139669968+pooja180801@users.noreply.github.com>
2024-02-10 18:58:31 +0530

    updated product service implementation

--- backend/src/main/java/com/seng22212project/bitebliss/services/impl/ProductServiceimpl.java
+++ backend/src/main/java/com/seng22212project/bitebliss/services/impl/ProductServiceimpl.java
@@ -1,18 +1,18 @@
 package com.seng22212project.bitebliss.services.impl;
 
-import com.example.demo.Exception.ResourceNotFoundException;
+import com.example.demo.dtos.CategoryDto;
 import com.example.demo.dtos.ProductDto;
+import com.example.demo.exceptions.CategoryNotFoundException;
+import com.example.demo.exceptions.ProductNotFoundException;
 import com.example.demo.models.Category;
 import com.example.demo.models.Products;
 import com.example.demo.repositories.CategoryRepository;
 import com.example.demo.repositories.ProductRepository;
 import com.example.demo.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,31 +26,62 @@ public class ProductServiceimpl implements ProductService {
 
 
     @Override
-    public ProductDto create(ProductDto products) {
+    public ProductDto create(ProductDto productDto,int cid) {
 
-        Products entity=toEntity(products);
-        Products save=productRepository.save(entity);
-        ProductDto dto=toDto(save);
+        Category cat =this.categoryRepository.findById(cid).orElseThrow(()->new CategoryNotFoundException("the category with this category id cannot be found"));
+
+        Products product=toEntity(productDto);
+        product.setCategory(cat);
+        Products save=this.productRepository.save(product);
+         ProductDto dto=toDto(save);
         return dto;
 
-    }
+        }
 
 
 
     @Override
-    public List<ProductDto> viewAllProducts(){
+  public List<ProductDto> viewAllProducts(){
         List<Products> findALL= productRepository.findAll();
         List<ProductDto> findAllDto= findALL.stream().map(products -> this.toDto(products)).collect(Collectors.toList());
-        return findAllDto;
-    }
+      return findAllDto;
+   }
 
     @Override
     public ProductDto viewProductById(int pid)
     {
-        Products findById=productRepository.findById(pid).orElseThrow(()->new ResourceNotFoundException("product with this product id " +pid+" is not found"));
+        Products findById=productRepository.findById(pid).orElseThrow(()->new ProductNotFoundException("product with this product id " +pid+" is not found"));
         ProductDto productDto = this.toDto(findById);
         return productDto;
     }
+
+    @Override
+    public List<ProductDto> getProductByCategory(int cid) {
+        Category cat=this.categoryRepository.findById(cid).orElseThrow(()->new CategoryNotFoundException("the category with this category id cannot be found"));
+        List<ProductDto> findByCategory=this.productRepository.findByCategory(cat).stream().map(products -> toDto(products)).collect(Collectors.toList());
+        return findByCategory;
+    }
+
+    @Override
+    public ProductDto updateProduct(int pid, ProductDto updatedProductDto) {
+
+            Products existingProduct = productRepository.findById(pid)
+                    .orElseThrow(() -> new ProductNotFoundException("Product not found with ID: " + pid));
+
+
+            existingProduct.setProduct_name(updatedProductDto.getProduct_name());
+            existingProduct.setPrice(updatedProductDto.getPrice());
+            existingProduct.setDescription(updatedProductDto.getDescription());
+            existingProduct.setImageUrl(updatedProductDto.getImageUrl());
+
+
+
+            Products updatedProduct =this.productRepository.save(existingProduct);
+
+
+            return toDto(updatedProduct);
+        }
+
 
     //productDto to product
     @Override
@@ -74,6 +105,16 @@ public class ProductServiceimpl implements ProductService {
         productDto.setDescription(products.getDescription());
         productDto.setPrice(products.getPrice());
         productDto.setImageUrl(products.getImageUrl());
+
+       //change categoty to categoryDto
+
+        CategoryDto categoryDto = new CategoryDto();
+
+        categoryDto.setCategory_id(products.getCategory().getCategory_id());
+        categoryDto.setCategory_name(products.getCategory().getCategory_name());
+
+        //setting categoryDto in productDto
+        productDto.setCategoryDto(categoryDto);
         return productDto;
     }
 
