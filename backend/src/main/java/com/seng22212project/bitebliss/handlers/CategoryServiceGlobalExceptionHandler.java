d6933d7e884817469778838731611c8ab7be0cc0
poojak <139669968+pooja180801@users.noreply.github.com>
2024-02-10 18:44:08 +0530

    added CategoryServiceGlobalExceptionHandler.java

---
+++ backend/src/main/java/com/seng22212project/bitebliss/handlers/CategoryServiceGlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.seng22212project.bitebliss.handlers;
+
+import com.example.demo.exceptions.CategoryNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+    public class CategoryServiceGlobalExceptionHandler {
+        @ExceptionHandler(CategoryNotFoundException.class)
+        public ResponseEntity<String> handleCategoryNotFoundException(CategoryNotFoundException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
