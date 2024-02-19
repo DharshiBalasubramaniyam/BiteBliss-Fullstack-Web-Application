45dcf65c274d97f05250c940e1e84677801e48ee
poojak <139669968+pooja180801@users.noreply.github.com>
2024-02-19 10:35:11 +0530

    updated categoryController.java

--- backend/src/main/java/com/seng22212project/bitebliss/controllers/CategoryController.java
+++ backend/src/main/java/com/seng22212project/bitebliss/controllers/CategoryController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categories")
+@RequestMapping("/bitebliss/categories")
+@CrossOrigin("*")
 public class CategoryController {
 
     @Autowired
