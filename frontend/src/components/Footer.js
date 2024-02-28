8097d34b7c0aeff029f768a1308e848b48306be7
dharshib <Dharshib.8@gmail.com>
2024-02-28 09:13:26 +0530

    add footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
-const Footer = () => {
-  return (
-    <div className="footer">
-      <h1 className="text-center">All Right Reserved &copy; BiteBliss</h1>
-      <p className="text-center mt-3">
-        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>
-        
-      </p>
-    </div>
-  );
-};
+import useCategories from "../hooks/useCategories";
+import '../assets/styles/footer.css'
+
+function Footer() {
+
+    const [categories] = useCategories();
+
+    return(
+        <footer>
+            <div>
+                <h1>About Us</h1>
+                <p>We create a warm and welcoming atmosphere 
+                    where every guest feels like family, and 
+                    where the quality of our food and service 
+                    is unmatched. Our goal is to be a restaurant 
+                    that not only nourishes the body, but also the soul.
+                </p>
+                <button>Learn more</button>
+            </div>
+
+            <div>
+                <h1>Our Menu</h1>
+                <ul>
+                    <li><a href="#">hello</a></li><li><a href="#">hello</a></li><li><a href="#">hello</a></li><li><a href="#">hello</a></li><li><a href="#">hello</a></li><li><a href="#">hello</a></li>
+                    {
+                        categories.map(cat => {
+                            return  <li key={cat.category_id}>
+                                        <a href={"#" + cat.category_name} className="nav-link">
+                                            {cat.category_name}
+                                        </a>
+                                    </li>
+                        })
+                    }
+                </ul>
+            </div>
+
+            <div>
+                <h1>Contact Us</h1>
+                <div>
+                <p><span><i className="fa fa-map-marker"></i></span>No 123, Lotus strret, Colombo</p>
+                <p><span><i className="fa fa-phone"></i></span>+94 741258963, 9632587412</p>
+                <p><span><i className="fa fa-envelope"></i></span>bitebliss@abc.com</p>
+                </div>
+                <p>
+                    <span><i className="fa fa-facebook"></i></span>
+                    <span><i className="fa fa-twitter"></i></span>
+                    <span><i className="fa fa-instagram"></i></span>
+                    <span><i className="fa fa-youtube"></i></span>
+                </p>
+            </div>
+        </footer>
+    )
+}
 
 export default Footer;
