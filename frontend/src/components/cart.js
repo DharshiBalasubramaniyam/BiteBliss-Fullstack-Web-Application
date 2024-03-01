bd8d029214da475bc3286af26dc15846603d9b6e
dharshib <Dharshib.8@gmail.com>
2024-03-01 08:54:04 +0530

    update cart styles

--- frontend/src/components/cart.js
+++ frontend/src/components/cart.js
@@ -1,102 +1,170 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/styles/cart.css";
-import { AiFillCloseCircle } from "react-icons/ai";
+import { AiOutlineClose, AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import API_BASE_URL from "../api-service/apiConfig";
+import axios from 'axios';
+import AuthService from "../api-service/authService";
 
 // have to update something in header.js
-const cart = ({ onClose }) => {
-  const [products, setProducts] = useState();
+const Cart = ({ isCartOpen, onClose, setNoOfCartItemsInHeader }) => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [noOfCartItems, setNoOfCartItems] = useState(0);
+  const [subtotal, setSubtotal] = useState(0.0);
+
   const fetchCartItems = async () => {
+    setLoading(true)
     try {
-      const response = await fetch("/cart");
-      const cartData = await response.json();
-      setProducts(cartData.products);
+      const response = await axios.get(API_BASE_URL + '/user/cart/getCartItems',
+        {
+          headers: AuthService.authHeader(),
+          params: {
+            email: AuthService.getCurrentUser().email
+          }
+        }).then(
+          (response) => {
+            console.log(response);
+            if (response.data.status === "SUCCESS") {
+              setProducts(response.data.response.cartItemResponse)
+              setNoOfCartItems(response.data.response.noOfCartItems)
+              setNoOfCartItemsInHeader(response.data.response.noOfCartItems)
+              setSubtotal(response.data.response.subtotal)
+              return
+            }
+          },
+          (error) => {
+            console.log(error);
+          }
+        )
     } catch (error) {
       console.error("Error fetching cart items:", error);
     }
+    setLoading(false)
   };
 
-  const onQuantityChange = async (productId, newQuantity) => {
+  const onQuantityChange = async (productId, quantity) => {
+    setLoading(true)
     try {
-      const response = await fetch("/cart", {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ productId, quantity: newQuantity }),
-      });
-      if (response.ok) {
-        await fetchCartItems();
-      } else {
-        console.error("Error updating cart:", response.statusText);
-      }
+      const response = await axios.put(API_BASE_URL + '/user/cart/update',
+        {
+          productId: productId,
+          quantity: quantity
+        },
+        {
+          headers: AuthService.authHeader(),
+          params: {
+            email: AuthService.getCurrentUser().email
+          }
+        }).then(
+          (response) => {
+            console.log(response);
+            if (response.data.status === "SUCCESS") {
+              window.location.reload()
+              return
+            }
+          },
+          (error) => {
+            console.log(error);
+          }
+        )
     } catch (error) {
-      console.error("Error updating quantity:", error);
+      console.error("Error fetching cart items:", error);
     }
+    setLoading(false)
   };
 
-  const onProductRemove = async (product) => {
+  const onProductRemove = async (cartItemId) => {
+    setLoading(true)
     try {
-      const response = await fetch("/cart/" + product.id, { method: "DELETE" });
-      if (response.ok) {
-        await fetchCartItems();
-      } else {
-        console.error("Error removing product:", response.statusText);
-      }
+      const response = await axios.delete(API_BASE_URL + '/user/cart/delete',
+        {
+          headers: AuthService.authHeader(),
+          params: {
+            cartItemId: cartItemId
+          }
+        }).then(
+          (response) => {
+            console.log(response);
+            if (response.data.status === "SUCCESS") {
+              window.location.reload()
+              return
+            }
+          },
+          (error) => {
+            console.log(error);
+          }
+        )
     } catch (error) {
-      console.error("Error removing product:", error);
+      console.error("Error fetching cart items:", error);
     }
+    setLoading(false)
   };
+
+  useEffect(() => {
+    fetchCartItems()
+  }, []);
 
   return (
-    <div className="shoppingCart">
+    <div className={isCartOpen ? "shoppingCart active" : "shoppingCart"}>
       <div className="header">
-        <h2>Shopping cart</h2>
-        <button className="btn close-btn" onClick={onClose}>
-          <AiFillCloseCircle size={30} />
-        </button>
+        <h2>Your cart</h2>
+        <div className="btn close-btn" onClick={onClose}>
+          <AiOutlineClose size={20} />
+        </div>
       </div>
       <div className="cart-products">
         {products.length === 0 && (
-          <span className="empty-text">No items in your cart</span>
+          <span className="empty-text">{isLoading ? "Loading..." : "Your cart is empty!"}</span>
         )}
-        {products.map((product) => (
-          <div className="cart-product" key={product.id}>
-            <img src={product.image} alt={product.name} />
+        {products.map((cartItem) => (
+          <div className="cart-product" key={cartItem.cartItemId}>
+            <img src={require(`../assets/images/${cartItem.imageUrl}`)} alt={cartItem.productName} />
             <div className="product-info">
-              <h3>{product.name}</h3>
+              <h4>
+                {cartItem.productName}
+                <div
+                  className={cartItem.quantity === 20 || isLoading ? "btn close-btn disable" : "btn close-btn"}
+                  onClick={() => onProductRemove(cartItem.cartItemId)}
+                >
+                  <RiDeleteBin6Line size={20} className="" />
+                </div>
+
+              </h4>
               <span className="product-price">
-                {product.price * product.count}$
+                {cartItem.price} x {cartItem.quantity} = Rs. {cartItem.totalPrice}
               </span>
+              <div className="quantity-control">
+                <span
+                  className={cartItem.quantity === 1 || isLoading ? "disable" : ""}
+                  onClick={() => onQuantityChange(cartItem.product_id, -1)}
+                >
+                  <AiOutlineMinus size={18} />
+                </span>
+                <span className="count">{cartItem.quantity}</span>
+                <span
+                  className={cartItem.quantity === 20 || isLoading ? "disable" : ""}
+                  onClick={() => onQuantityChange(onQuantityChange(cartItem.product_id, 1))}
+                >
+                  <AiOutlinePlus size={18} />
+                </span>
+              </div>
+
             </div>
-            <div className="quantity-control">
-              <button
-                className="btn quantity-btn"
-                onClick={() => onQuantityChange(product.id, product.count - 1)}
-                disabled={product.count <= 1}
-              >
-                -
-              </button>
-              <span className="count">{product.count}</span>
-              <button
-                className="btn quantity-btn"
-                onClick={() => onQuantityChange(product.id, product.count + 1)}
-              >
-                +
-              </button>
-            </div>
-            <button
-              className="btn remove-btn"
-              onClick={() => onProductRemove(product)}
-            >
-              <RiDeleteBin6Line size={20} />
-            </button>
+
           </div>
         ))}
+      </div>
+      <div className="cart-summary">
         {products.length > 0 && (
-          <button className="btn checkout-btn">Proceed to checkout</button>
+          <>
+            <h3>Subtotal: Rs. {subtotal}</h3>
+            <button className="btn checkout-btn">Proceed to checkout</button>
+          </>
         )}
       </div>
     </div>
   );
 };
 
-export default cart;
+export default Cart;
